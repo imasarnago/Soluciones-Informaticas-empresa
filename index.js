be4072f98d237d098f1aa2467dcc782e2f8df7ae
imanol <imanolsarnago@gmail.com>
2023-04-17 18:36:49 -0300

    Mejora en la validación del primer formulario

--- index.js
+++ index.js
@@ -10,28 +10,20 @@ const cambiaColor = document.getElementById("cambiaColor")
 // El codigo a continuación lo que hace es hacer un formulario paso a paso. Sus valores se pueden modificar siguiendo la misma lógica con otros campos input
 
 enviarBtn.addEventListener("click", function(event) {
-    event.preventDefault()
-    let email = document.querySelector("#exampleFormControlInput1").value;
-    let texto = document.querySelector("#exampleFormControlTextarea1").value;
-
-    if (email === "" || texto === ""){
-      alert("Los campos son requeridos")
-    }else {
-      summary.innerHTML = `Email: ${email}<br> Comentario: ${texto}`;
     
-      // ocultar el primer paso y mostrar el segundo paso
-      form1.style.display = "none";
-      form2.style.display = "block";
-      tituloReclamo.style.display = "none"
-      cambiaColor.style.color = "#ffc300"
-    }
-
   });
 
 
 
 $(document).ready(function () {
-  // Esto se ejecuta luego de cargar el documento. Es como un defer
+
+
+
+
+
+
+  // La siguiente función me convierte a PDF el resúmen de los datos ingresados por el usuario
+  
   $("#printSummary").click(function () {
     var printContents = $("#summaryContent").html(); //Tomo el contenido del Summary
     w = window.open(); //Abro una nueva ventana
@@ -71,3 +63,23 @@ $("#enviar-2").click(function(){
     $("#form4").submit()
   }
 })
+
+
+function subirForm(event) {
+    event.preventDefault()
+    let email = document.querySelector("#exampleFormControlInput1").value;
+    let texto = document.querySelector("#exampleFormControlTextarea1").value;
+
+    if (email === "" || texto === ""){
+      alert("Los campos son requeridos")
+    }else {
+      summary.innerHTML = `Email: ${email}<br> Comentario: ${texto}`;
+    
+      // ocultar el primer paso y mostrar el segundo paso
+      form1.style.display = "none";
+      form2.style.display = "block";
+      tituloReclamo.style.display = "none"
+      cambiaColor.style.color = "#ffc300"
+    }
+
+}
